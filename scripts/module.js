43347e637d80adf92a0727e66b09240ade6c0d28
Cody Swendrowski <cody@ironmoosedevelopment.com>
2021-01-12 20:34:08 -0600

    Add monkeypatch and manifest

--- scripts/module.js
+++ scripts/module.js
@@ -1,9 +1,32 @@
-(() => { })();
-
 Hooks.once('init', async function() {
+    TextEditor.create = async function (options={}, content="") {
+        const mceConfig = mergeObject(CONFIG.TinyMCE, options, {inplace: false});
+        mceConfig.target = options.target;
 
-});
+        // The patch
+        mceConfig.file_picker_callback = function (pickerCallback, value, meta) {
+            var filePicker = new FilePicker({
+                callback: path => {
+                    pickerCallback(path);
+                },
+            });
+            filePicker.render();
+        };
 
-Hooks.once('ready', async function() {
-
+        if ( mceConfig.content_css instanceof Array ) {
+            mceConfig.content_css = mceConfig.content_css.map(c => getRoute(c)).join(",");
+        }
+        mceConfig.init_instance_callback = editor => {
+            const window = editor.getWin();
+            if ( content ) editor.setContent(content);
+            window.addEventListener("wheel", event => {
+            if ( event.ctrlKey ) event.preventDefault();
+            }, {passive: false});
+            window.addEventListener("drop", ev => this._onDropEditorData(ev, editor))
+        };
+    
+        const editors = await tinyMCE.init(mceConfig);
+        console.log(editors);
+        return editors[0];
+    }
 });
